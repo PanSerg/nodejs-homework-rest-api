47667803c1bf9177a7401a297c94dd290c602fba
Sergey Opanasenko <svopanasenko22@gmail.com>
2023-05-24 00:30:04 +0200

    add Schema of model

---
+++ schemas/contactsSchema.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+const handleMongooseError = require('../helpers/handleMongooseError');
+
+const contactSchemaDb = new Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Set name for contact"],
+        },
+        email: {
+            type: String,
+            required: [true, "Set email for contact"],
+        },
+        phone: {
+            type: String,
+            match: /^[0-9]{10}$/,
+            required: [true, "Set phone number for contact"],
+        },
+        favorite: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    {
+        versionKey: false,
+        timestamps: true
+    },
+);
+
+contactSchemaDb.post("save", handleMongooseError);
+
+const Contact = model('Contact', contactSchemaDb);
+
+module.exports = {Contact};
