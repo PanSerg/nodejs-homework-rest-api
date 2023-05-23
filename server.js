ee1728ce6e0bd1523aed8ccb4e820c3a7858d849
Sergey Opanasenko <svopanasenko22@gmail.com>
2023-05-23 20:08:38 +0200

    init project

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+const mongoose = require('mongoose');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require('./app')
+// M54Mtg0IZP1O0Bj6
+const DB_HOST =
+  "mongodb+srv://Serhii:M54Mtg0IZP1O0Bj6@cluster0.xg8oslj.mongodb.net/my_contacts?retryWrites=true&w=majority";
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Database connection successful");
+    })
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  });
+  
