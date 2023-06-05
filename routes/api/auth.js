ed6a94180a4020df20673877a78cdd62eb88579d
Sergey Opanasenko <svopanasenko22@gmail.com>
2023-06-05 17:11:07 +0200

    fix HttpEr

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const ctrl = require("../../controllers/Users/index");
+const ctrl = require("../../controllers/Users");
 const { validateBody, authenticate } = require("../../middlewares");
 const { authSchema } = require("../../schemas/contacts");
 
