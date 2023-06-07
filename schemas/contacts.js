18e19aafbcffb698eb600ee310e10c212325362b
Sergey Opanasenko <svopanasenko22@gmail.com>
2023-06-07 16:49:06 +0200

    del name

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -14,7 +14,6 @@ const updateFavoriteSchema = Joi.object({
 });
 
 const registerSchema = Joi.object({
-  name: Joi.string().required(),
   email: Joi.string().required(),
   password: Joi.string().min(6).required(),
 });
