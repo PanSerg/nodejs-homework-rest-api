85ad676729e1a064e40d9ec35d33a68c899b9e86
Sergey Opanasenko <svopanasenko22@gmail.com>
2023-06-14 17:58:17 +0200

    add avatar and favorit

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,9 +4,15 @@ const HttpError = require("../helpers/HttpError");
 
 const listContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 10, favorite } = req.query;
   const skip = (page - 1) * limit;
-  const result = await Contact.find({ owner}, "-createdAt -updatedAt -", {skip, limit});
+
+  const query = favorite ? { owner, favorite } : { owner };
+
+  const result = await Contact.find(query, "-createdAt -updatedAt -", {
+    skip,
+    limit
+  });
   res.json(result);
 };
 
